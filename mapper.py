49b85c1bd7ff4740987c9d85c47e8e342ccff723
Aiden Bromaghin <aibromaghin@gmail.com>
2021-09-06 03:31:05 +0000

    lowercase, naive stopword removal

--- mapper.py
+++ mapper.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 import sys
 
+# create set of stopwords 
+stops = set(['i', 'the', 'and', ',', '.', ';', '?', '!', 'a', 'me', 'my', 'you', 'your', 'are', 'be', 'to', 'or', 'but', 'he', 'her', 'his', 'hers', 'with', 'for', 'by'])
+
 # get all lines from stdin
 for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
+    # remove leading and trailing whitespace and convert to lowercase
+    line = line.strip().lower()
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
-    # output tuples (word, 1) in tab-delimited format
+    # output tuples (word, 1) in tab-delimited format after removing stopwords
     for word in words:
-        print '%s\t%s' % (word, "1")
+	if word not in stops:
+             print '%s\t%s' % (word, "1")
